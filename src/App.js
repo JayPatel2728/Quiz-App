c1e6072e0ffcba40d6c9f76ce2fc8d1d75547893
Jay <jay.patel20032@gmail.com>
2022-07-29 10:40:43 -0400

    cleaned up code and added icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { useState, useEffect } from "react"
-import { nanoid } from "nanoid"
-import Question from "./components/Question"
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Question from "./components/Question";
 
 function App() {
-  const [startGame, setStartGame] = useState(false)
-  const [quizData, setQuizData] = useState()
-  const [checkedAnswers, setCheckedAnswers] = useState(false)
-  const [score, setScore] = useState(0)
-  const [startNewGame, setStartNewGame] = useState(false)
+  const [startGame, setStartGame] = useState(false);
+  const [quizData, setQuizData] = useState();
+  const [checkedAnswers, setCheckedAnswers] = useState(false);
+  const [score, setScore] = useState(0);
+  const [startNewGame, setStartNewGame] = useState(false);
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy")
-    .then(response => response.json())
-    .then(data => setQuizData(trimValues(data.results)))
-  }, [startNewGame])
-
+    fetch("https://opentdb.com/api.php?amount=5&category=31&difficulty=easy")
+      .then((response) => response.json())
+      .then((data) => setQuizData(trimValues(data.results)));
+  }, [startNewGame]);
+  
   function trimValues(arr) {
-    const newArr = []
+    const newArr = [];
     arr.forEach((element) => {
       newArr.push({
         id: nanoid(),
@@ -36,75 +36,83 @@ function App() {
             reveal: false,
           })),
         ]),
-      })
-    })
+      });
+    });
     return newArr;
   }
 
   function flipStartGame() {
-    setStartGame((prevState) => !prevState)
+    setStartGame((prevState) => !prevState);
   }
 
   function shuffle(array) {
-    let currentIndex = array.length, randomIndex
+    let currentIndex = array.length,
+      randomIndex;
     while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex--
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
     return array;
   }
 
   function handleClick(question, answer) {
-    const prevData = [...quizData]
-    const updatedArray = []
+    const prevData = [...quizData];
+    const updatedArray = [];
     for (let i = 0; i < prevData.length; i++) {
       if (prevData[i].question === question) {
         for (let j = 0; j < prevData[i].answers.length; j++) {
           if (prevData[i].answers[j].value === answer) {
-            prevData[i].answers[j].isSelected = !prevData[i].answers[j].isSelected;
+            prevData[i].answers[j].isSelected =
+              !prevData[i].answers[j].isSelected;
           } else {
             prevData[i].answers[j].isSelected = false;
           }
         }
-        updatedArray.push(prevData[i])
+        updatedArray.push(prevData[i]);
       } else {
-        updatedArray.push(prevData[i])
+        updatedArray.push(prevData[i]);
       }
     }
-    setQuizData(updatedArray)
+    setQuizData(updatedArray);
   }
 
   function cleanString(val) {
-    let newStr = val.replace(/&quot;/g, "'")
-    newStr = newStr.replace(/&#/g, " #")
-    newStr = newStr.replace(/;s/g, "")
-    newStr = newStr.replace(/&amp;/g, "")
-    return newStr
+    let newStr = val.replace(/&quot;/g, "'");
+    newStr = newStr.replace(/&#/g, " #");
+    newStr = newStr.replace(/;s/g, "");
+    newStr = newStr.replace(/&amp;/g, "");
+    return newStr;
   }
 
   function scoreQuiz() {
     setQuizData((prevData) => {
-      const updatedArray = []
+      const updatedArray = [];
       for (let i = 0; i < prevData.length; i++) {
         for (let j = 0; j < prevData[i].answers.length; j++) {
-          prevData[i].answers[j].reveal = true
-          if (prevData[i].answers[j].isSelected && prevData[i].answers[j].value === prevData[i].correct_answer) {
-            setScore((prevScore) => prevScore + 1)
+          prevData[i].answers[j].reveal = true;
+          if (
+            prevData[i].answers[j].isSelected &&
+            prevData[i].answers[j].value === prevData[i].correct_answer
+          ) {
+            setScore((prevScore) => prevScore + 1);
           }
         }
-        updatedArray.push(prevData[i])
+        updatedArray.push(prevData[i]);
       }
-      return updatedArray
-    })
-    setCheckedAnswers(true)
+      return updatedArray;
+    });
+    setCheckedAnswers(true);
   }
 
   function restartGame() {
-    setQuizData(null)
-    setCheckedAnswers(false)
-    setStartNewGame((prevValue) => !prevValue)
-    setScore(0)
+    setQuizData(null);
+    setCheckedAnswers(false);
+    setStartNewGame((prevValue) => !prevValue);
+    setScore(0);
   }
 
   let quizElements = null;
@@ -117,12 +125,11 @@ function App() {
         answers={item.answers}
         handleClick={handleClick}
       />
-    ))
+    ));
   }
 
   return (
     <main>
-
       {!startGame && (
         <div className="start-screen">
           <h2>Quizzical</h2>
